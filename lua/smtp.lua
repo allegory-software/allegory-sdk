4b3840ce2b92ce2d5de93d4e21c265e476cbdb65
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2022-02-08 02:51:19 +0200

    smtp client refactoring and fixing (still no TLS yet)

--- lua/smtp.lua
+++ lua/smtp.lua
@@ -3,15 +3,14 @@
 	Async SMTP(s) client.
 	Written by Cosmin Apreutesei. Public Domain.
 
-API
-	...
+	smtp:connect{host=, port=, tls=, user=, pass=} -> c
+	c:sendmail{from=, to=, headers=, message=} -> c
+	c:close() -> c
 
 ]=]
 
 if not ... then require'smtp_test'; return end
 
-local ffi = require'ffi'
-local clock = require'time'.clock
 local glue = require'glue'
 local errors = require'errors'
 local linebuffer = require'linebuffer'
@@ -19,254 +18,163 @@ local b64 = require'base64'.encode
 local _ = string.format
 
 local client = {
+	libs = 'sock sock_libtls',
 	type = 'smtp_client', debug_prefix = 'S',
 	host = '127.0.0.1',
-	port = 25,
+	port = 587,
 	connect_timeout = 5,
+	sendmail_timeout = 60,
 	domain = 'localhost', --client's domain
+	max_line_size = 8192,
 }
 
-function client:bind_libs(libs)
-	for lib in libs:gmatch'[^%s]+' do
+local check_io, checkp, check, protect = errors.tcp_protocol_errors'smtp'
+
+function client:connect(t)
+
+	local self = glue.object(self, {}, t)
+
+	for lib in self.libs:gmatch'[^%s]+' do
 		if lib == 'sock' then
 			local sock = require'sock'
-			self.create_tcp    = sock.tcp
-			self.cowrap        = sock.cowrap
-			self.newthread     = sock.newthread
-			self.suspend       = sock.suspend
-			self.resume        = sock.resume
-			self.thread        = sock.thread
-			self.start         = sock.start
-			self.sleep         = sock.sleep
-			self.currentthread = sock.currentthread
+			self.create_tcp = sock.tcp
+			self.clock      = sock.clock
+			self.resolve = self.resolve
+				or function(_, host)
+					return sock.addr(host, 0):tostring()
+				end
 		elseif lib == 'sock_libtls' then
 			local socktls = require'sock_libtls'
-			self.stcp          = socktls.client_stcp
-			self.stcp_config   = socktls.config
+			self.create_stcp = socktls.client_stcp
+			self.stcp_config = socktls.config
 		else
 			assert(false)
 		end
 	end
-end
 
---error handling -------------------------------------------------------------
+	self.host_addr = check(self, self:resolve(self.host))
+	self.tcp = check(self, self.create_tcp())
 
-local check_io, checkp, check, protect = errors.tcp_protocol_errors'smtp'
+	--debugging
 
-client.check_io = check_io
-client.checkp = checkp
-client.check = check
-
-function client:protect(method)
-	local function oncaught(err)
+	local function mprotect(method)
+		local oncaught
 		if self.debug and self.debug.errors then
-			self:log('ERROR', 'smtp', method, '%s', err)
-			self.logged = true
+			local log = self.log or require'logging'.log
+			function oncaught(err)
+				log('ERROR', 'smtp', method, '%s', err)
+			end
 		end
+		self[method] = protect(self[method], oncaught)
 	end
-	self[method] = protect(self[method], oncaught)
-end
-
---low-level I/O API ----------------------------------------------------------
-
-function client:send(buf, sz)
-	self:check_io(self.tcp:send(buf, sz, self.send_expires))
-end
-
-function client:send_line(fmt, s)
-	self:send(_(fmt, s) .. '\r\n')
-end
-
-function client:close(expires)
-	self:check_io(self.tcp:close(expires))
-end
-
---linebuffer-based read API --------------------------------------------------
-
-function client:read_exactly(n, write)
-	local read = self.linebuffer.read
-	local n0 = n
-	while n > 0 do
-		local buf, sz = read(n)
-		self:check_io(buf, sz)
-		write(buf, sz)
-		n = n - sz
-	end
-end
-
-function client:read_line()
-	return self:check_io(self.linebuffer.readline())
-end
-
-function client:read_until_closed(write_content)
-	local read = self.linebuffer.read
-	while true do
-		local buf, sz = read(1/0)
-		if not buf then
-			self:check_io(nil, sz)
-		elseif sz == 0 then
-			break
-		end
-		write_content(buf, sz)
-	end
-end
-
-client.max_line_size = 8192
-
-function client:create_linebuffer()
-	local function read(buf, sz)
-		return self.tcp:recv(buf, sz, self.read_expires)
-	end
-	self.linebuffer = linebuffer(read, '\r\n', self.max_line_size)
-end
-
---instantiation --------------------------------------------------------------
-
-function client:log(severity, module, event, fmt, ...)
-	local logging = self.logging
-	if not logging or logging.filter[severity] then return end
-	local S = self.tcp or '-'
-	local s = fmt and _(fmt, logging.args(...)) or ''
-	logging.log(severity, module, event, '%-4s %s', S, s)
-end
-
-function client:new(t)
-
-	local self = glue.object(self, {}, t)
-
-	self:bind_libs(self.libs)
 
 	if self.debug and self.debug.tracebacks then
 		self.tracebacks = true --for tcp_protocol_errors.
 	end
 
-	if self.debug and (self.logging == nil or self.logging == true) then
-		self.logging = require'logging'
-	end
-
-	if self.debug and self.debug.protocol then
-
-		function self:dp(...)
-			return self:log('', 'smtp', ...)
-		end
-
-	else
-		self.dp = glue.noop
-	end
-
-	self:create_linebuffer()
-	return self
-end
-
-function client:check_reply(match)
-	while true do
-		local s = self:check_io(self:read_line())
-		local code, sep = s:match'^(%d%d%d)(.?)'
-		self:checkp(tonumber(code), 'invalid response line: %s', s)
-		if sep == ' ' then
-			self:checkp(code:match(match),
-				'unexpected response %s (expected it to match "%s")', s, match)
-			break
-		end
-	end
-end
-
-function client:send_mail(req)
-	self:check_reply'2..'
-	self:send_line('EHLO %s', self.domain)
-	self:check_reply'2..'
-	--[[
-		--TODO:
-		self.try(self.tp:command("AUTH", "LOGIN"))
-		self.try(self.tp:check("3.."))
-		self.try(self.tp:send(mime.b64(user) .. "\r\n"))
-		self.try(self.tp:check("3.."))
-		self.try(self.tp:send(mime.b64(password) .. "\r\n"))
-		return self.try(self.tp:check("2.."))
-	]]
-	self:send_line('AUTH PLAIN %s', b64('\0' .. self.user .. '\0' .. self.pass))
-	self:check_reply'2..'
-	self:send_line('MAIL FROM: %s', req.from)
-	self:check_reply'2..'
-	local to = type(req.to) == 'string' and {req.to} or req.to
-	for i,to in ipairs(to) do
-		 self:send_line('RCPT TO: %s', req.to)
-		 self:check_reply'2..'
-	end
-	self:send_line'DATA'
-	self:check_reply'3..'
-	local ht = glue.update({}, req.headers)
-	ht.date = os.date('!%a, %d %b %Y %H:%M:%S -0000')
-	ht['x-mailer'] = 'allegory-sdk smtp client'
-	ht['mime-version'] = '1.0'
-	local t = {}
-	for k,v in glue.sortedpairs(ht) do
-		t[#t+1] = k:lower() .. ': ' .. tostring(v)
-	end
-	t[#t+1] = ''
-	t[#t+1] = ''
-	local headers = table.concat(t, '\r\n')
-	self:send(headers)
-	self:send(req.message)
-	self:send'\r\n.\r\n'
-	self:check_reply'2..'
-end
-
-function client:resolve(host) --stub (use a resolver)
-	return host
-end
-
-function client:connect()
-	self.tcp = self:check(self.create_tcp())
-
 	if self.debug and self.debug.stream then
-
-		local function ds(event, s)
-			self:log('', 'smtp', event, '%5s %s', s and #s or '', s or '')
-		end
-
-		glue.override(self.tcp, 'recv', function(inherited, self, buf, ...)
-			local sz, err = inherited(self, buf, ...)
-			if not sz then return nil, err end
-			ds('<', ffi.string(buf, sz))
-			return sz
-		end)
-
-		glue.override(self.tcp, 'send', function(inherited, self, buf, sz, ...)
-			local ok, err = inherited(self, buf, sz, ...)
-			if not ok then return nil, err end
-			ds('>', ffi.string(buf, sz or #buf))
-			return ok
-		end)
-
-		glue.override(self.tcp, 'close', function(inherited, self, ...)
-			local ok, err = inherited(self, ...)
-			if not ok then return nil, err  end
-			ds('CC')
-			return ok
-		end)
-
+		self.tcp:debug'smtp'
 	end
 
-	local dt = self.connect_timeout
-	local expires = dt and clock() + dt or nil
-	self:check_io(self.tcp:connect(self:resolve(self.host), self.port, expires))
-	self:dp('connect', '%s', err or '')
-end
+	local dp = glue.noop
+	if self.debug and self.debug.protocol then
+		local log = self.log or require'logging'.log
+		function dp(...)
+			return log('', 'smtp', ...)
+		end
+	end
 
-function client:quit()
-	self:send_line'QUIT'
-	self:check_reply'2..'
-	self.tcp:close()
-end
+	--I/O
 
-function client:sendmail(req)
-	self:connect()
-	self:send_mail(req)
-	self:quit()
-	return true
-end
-client:protect'sendmail'
+	local expires
+	local function set_timeout(dt)
+		expires = dt and self.clock() + dt or nil
+	end
 
+	local function read(buf, sz)
+		return self.tcp:recv(buf, sz, expires)
+	end
+	local readline = linebuffer(read, '\r\n', self.max_line_size).readline
+
+	local function send(buf, sz)
+		check_io(self, self.tcp:send(buf, sz, expires))
+	end
+
+	local function send_line(fmt, s)
+		s = _(fmt, s)
+		check(self, not s:find'[\r\n]', 'invalid line: %s', s)
+		send(s..'\r\n')
+	end
+
+	local function check_reply(match)
+		while true do
+			local s = check_io(self, readline())
+			local code, sep = s:match'^(%d%d%d)(.?)'
+			checkp(self, tonumber(code), 'invalid response line: %s', s)
+			if sep == ' ' then
+				checkp(self, code:find(match), 'unexpected response code: %s', s)
+				break
+			end
+		end
+	end
+
+	function self:_connect()
+		set_timeout(self.connect_timeout)
+		check_io(self, self.tcp:connect(self.host_addr, self.port, expires))
+		dp('connect', '%s %s', self.tcp, err or '')
+		check_reply'2..'
+		send_line('EHLO %s', self.domain)
+		check_reply'2..'
+		send_line('AUTH PLAIN %s', b64('\0' .. self.user .. '\0' .. self.pass))
+		check_reply'2..'
+		return self
+	end
+	mprotect'_connect'
+
+	function self:sendmail(req)
+		dp('sendmail', '%s from=%s to=%s subj="%s" msg#=%s', self.tcp,
+			req.from, req.to, req.headers and req.headers.subject,
+			glue.kbytes(#req.message))
+		set_timeout(self.sendmail_timeout)
+		send_line('MAIL FROM: %s', req.from)
+		check_reply'2..'
+		local to = type(req.to) == 'string' and {req.to} or req.to
+		for i,to in ipairs(to) do
+			 send_line('RCPT TO: %s', req.to)
+			 check_reply'2..'
+		end
+		send_line'DATA'
+		check_reply'3..'
+		local ht = glue.update({}, req.headers)
+		ht.date = os.date('!%a, %d %b %Y %H:%M:%S -0000')
+		ht['x-mailer'] = 'allegory-sdk smtp client'
+		ht['mime-version'] = '1.0'
+		local t = {}
+		for k,v in glue.sortedpairs(ht) do
+			t[#t+1] = k:lower() .. ': ' .. tostring(v)
+		end
+		t[#t+1] = ''
+		t[#t+1] = ''
+		local headers = table.concat(t, '\r\n')
+		send(headers)
+		send(req.message:gsub('^%.', '..'):gsub('\n%.', '..'))
+		send'\r\n.\r\n'
+		check_reply'2..'
+		return self
+	end
+	mprotect'sendmail'
+
+	function self:close()
+		dp('close', '%s', self.tcp)
+		set_timeout(self.connect_timeout)
+		send_line'QUIT'
+		check_reply'2..'
+		check_io(self, self.tcp:close(expires))
+		return true
+	end
+	mprotect'close'
+
+	return self:_connect()
+end
 
 return client
