d80413be4be1c21eb4121773155029f1de2facdc
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2022-02-08 11:47:39 +0200

    unimportant

--- tests/smtp_test.lua
+++ tests/smtp_test.lua
@@ -1,14 +1,15 @@
 
+local glue = require'glue'
 local smtp = require'smtp'
 local sock = require'sock'
 local logging = require'logging'
+require'ffi'.tls_libname = 'tls_bearssl'
 
 logging.debug = true
 
 sock.run(function()
 
-	local smtp = smtp:new{
-		libs = 'sock',
+	local smtp = smtp:connect{
 		debug = {
 			protocol = true,
 			errors = true,
@@ -16,11 +17,16 @@ sock.run(function()
 			stream = true,
 		},
 		logging = true,
-		domain = '"localhost"',
+		domain = 'localhost',
 		host = 'mail.bpnpart.com',
-		port = 587,
+		--port = 587,
+		port = 465, tls = true,
 		user = 'admin@bpnpart.com',
 		pass = 'Bpnpart@0@0',
+		tls_options = {
+			ca_file = 'x:/care/cacert.pem',
+			loadfile = glue.readfile,
+		},
 	}
 
 	assert(smtp:sendmail{
@@ -33,5 +39,7 @@ sock.run(function()
 			subject = 'How are you today?',
 		},
 	})
+
+	assert(smtp:close())
 
 end)
