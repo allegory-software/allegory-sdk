cf593e48911bcf4781b55190217bf6a90c545467
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2022-02-09 19:27:00 +0200

    webb_spa: client_config()

--- lua/webb_spa.lua
+++ lua/webb_spa.lua
@@ -5,22 +5,23 @@
 
 API
 
-	cssfile(file)                       add one or more css files to all.css
-	jsfile(file)                        add one or more js files to all.js
-	fontfile(file)                      add one or more font files to the preload list
-	css(s)                              add css code to inline.css
-	js(s)                               add js code to inline.js
-	html(s)                             add inline html code to the SPA body
+	client_config(name, [default])         make config value available on client-side
+	cssfile(file)                          add one or more css files to all.css
+	jsfile(file)                           add one or more js files to all.js
+	fontfile(file)                         add one or more font files to the preload list
+	css(s)                                 add css code to inline.css
+	js(s)                                  add js code to inline.js
+	html(s)                                add inline html code to the SPA body
 
-DEFINES
+ACTIONS
 
-	action['config.js']                 expose required config() values
-	action['strings.js']                load strings.<lang>.js
-	action['all.js']                    output of jsfile() calls
-	action['all.css']                   output of cssfile() calls
-	action['inline.js']                 output of js() calls
-	action['inline.css']                output of css() calls
-	action['404.html']                  SPA response
+	config.js        expose required config() values
+	strings.js       load strings.<lang>.js
+	all.js           output of jsfile() calls
+	all.css          output of cssfile() calls
+	inline.js        output of js() calls
+	inline.css       output of css() calls
+	404.html         SPA response
 
 LOADS
 
@@ -28,27 +29,47 @@ LOADS
 
 CONFIG
 
-	config'js_mode'                     'bundle' | 'ref' | 'separate'
-	config'css_mode'                    'bundle' | 'ref' | 'separate'
+	js_mode             'separate'   'bundle' | 'ref' | 'separate'
+	css_mode            'separate'   'bundle' | 'ref' | 'separate'
 
-	config'body_classes'                css classes for <body>
-	config'body_attrs'                  attributes for <body>
-	config'head'                        content for <head>
-	config'page_title_suffix'           title suffix, both client-side & server-side
-	config'infer_page_title'            server-side page title inferring f(body) -> s
-	config'favicon_href'                favicon url
-	config'client_action'               enable client routing (true).
-	config'aliases'                     aliases for client-side routing; defined in webb_action.
+	body_classes                     css classes for <body>
+	body_attrs                       attributes for <body>
+	head                             content for <head>
+	page_title_suffix                title suffix, both client-side & server-side
+	infer_page_title                 server-side page title inferring f(body) -> s
+	favicon_href                     favicon url
+	client_action       true         enable client routing.
+	aliases                          aliases for client-side routing; defined in webb_action.
 
-	config'facebook_app_id'             for webb.auth.facebook.js
-	config'google_client_id'            for webb.auth.google.js
-	config'analytics_ua'                for webb.analytics.js
+	facebook_app_id                  for webb.auth.facebook.js
+	google_client_id                 for webb.auth.google.js
+	analytics_ua                     for webb.analytics.js
 
 ]==]
 
 require'webb_action'
 
 local format = string.format
+
+local client_configs = {}
+
+function client_config(name, default)
+	if not client_configs[name] then
+		client_configs[name] = true
+		table.insert(client_configs, name)
+	end
+	return config(name, default)
+end
+
+client_config'app_name'
+client_config'lang'
+client_config'aliases'
+client_config'root_action'
+client_config'templates_action'
+client_config'page_title_suffix'
+client_config'facebook_app_id'
+client_config'analytics_ua'
+client_config'google_client_id'
 
 --pass required config values to the client
 action['config.js'] = function()
@@ -60,23 +81,12 @@ action['config.js'] = function()
 	config('root_action', 'en')
 	config('page_title_suffix', ' - '..host())
 
-	local function C(name)
-		if config(name) == nil then return end
-		out('config('
-			..cjson.encode(name)..', '
-			..cjson.encode(config(name))..')\n')
+	for i,k in ipairs(client_configs) do
+		local v = config(k)
+		if v ~= nil then
+			out(format('config(%s, %s)\n', json(k), json(v)))
+		end
 	end
-
-	C'app_name'
-	C'lang'
-	C'aliases'
-	C'root_action'
-	C'templates_action'
-	C'page_title_suffix'
-
-	C'facebook_app_id'
-	C'analytics_ua'
-	C'google_client_id'
 
 end
 
