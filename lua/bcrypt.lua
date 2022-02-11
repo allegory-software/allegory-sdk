7d2cb011ff50dfe100f9096de7a049957b0f1b55
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2022-02-11 02:14:22 +0200

    bcrypt

---
+++ lua/bcrypt.lua
@@ -0,0 +1,30 @@
+--[[
+
+	bcrypt binding.
+	Written by Cosmin Apreutesei. Public Domain.
+
+]]
+
+local ffi = require'ffi'
+local C = ffi.load'bcrypt1'
+
+ffi.cdef[[
+char *crypt(const char *key, const char *setting);
+char *crypt_gensalt(const char *prefix, unsigned long count,
+	const char *input, int size);
+]]
+
+local bcrypt = {}
+
+function bcrypt.crypt(key, secret)
+	local salt = C.crypt_gensalt('$2a$', 12, secret, #secret)
+	assert(salt ~= nil, 'secret too short')
+	return ffi.string(C.crypt('abcd', salt))
+end
+
+if not ... then
+	assert(bcrypt.crypt('dude', '0123456789012345') ==
+		'$2a$12$KBCwKxOzLha2MR.vKhKyLOPn6ktd5Jn14htId0DRB7/3RZF7.VbHu')
+end
+
+return bcrypt
