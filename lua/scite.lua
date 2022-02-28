da530dadd245c03a0993535b590a76b062a755c1
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2022-02-28 21:08:26 +0200

    unimportant

--- lua/scite.lua
+++ lua/scite.lua
@@ -1,16 +1,7 @@
-
 io.stdout:setvbuf'no'
-io.stderr:setvbuf'no'
-
-local logging = require'logging'
-function pr(...)
-	print(logging.printargs(...))
-	io.stdout:flush()
-	return ...
-end
-
 local glue = require'glue'
 local fs = require'fs'
 local tests_dir = fs.exedir()..'/../../tests'
 glue.luapath(tests_dir)
 fs.cd(tests_dir)
+require'$log'
