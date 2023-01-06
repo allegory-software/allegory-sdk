1110688ce71aae866a90056dbb16839c17899d5f
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2023-01-07 00:15:04 +0200

    css_report_specificity(); css `--inc` feature

--- www/divs.js
+++ www/divs.js
@@ -197,6 +197,10 @@
 		e.popup_fixed
 	lists:
 		e.make_list()
+	css dev tools:
+		css_report_specificity(file, max_spec)
+	composable css:
+		`--inc: cls1 ...;` css declarations.
 
 */
 
@@ -1400,11 +1404,12 @@ e.do_bind = function(on) {
 /* events & event wrappers ---------------------------------------------------
 
 NOTE: these wrappers block mouse events on any target that has attr `disabled`
-or that has any ancestor with attr `disabled`. `pointer-events: none` is not
-a solution because it makes click-through popups.
+or that has any ancestor with attr `disabled`. We're not using
+`pointer-events: none` because that makes disabled popups click-through.
 
 NOTE: preventing focusing is a matter of not-setting/removing attr `tabindex`
 except for input elements that must have an explicit `tabindex=-1`.
+This is not done here, see disablable_widget() and focusable_widget().
 
 */
 
@@ -2638,6 +2643,125 @@ method(Element, 'make_list', function() {
 	e.set_movable_element_pos = function(i, x, moving) {
 
 	}
+
+})
+
+// CSS specificity reporting -------------------------------------------------
+
+function css_selector_specificity(s0) {
+	let s = s0
+	let n = 0 // current specificity
+	let maxn = 0 // current max specificity
+	let cs = [0] // call stack: 1 for :is(), 0 for :not() and :where()
+	let ns = [] // specificity stack for :is()
+	let maxns = [] // max specificity stack for :is()
+	let z = 0 // call stack depth of first :where()
+	let sm // last matched string
+	function match(re) {
+		let m = s.match(re)
+		if (!m) return
+		sm = m[0]
+		assert(sm.len)
+		s = s.slice(sm.len)
+		return true
+	}
+	function next() {
+		if (!s.len) return max(maxn, n)
+		if (match(/^[ >+~*]+/ )) return next()
+		if (match(/^\)/      )) {
+			assert(cs.len > 1, 'unexpected )')
+			if (cs.pop()) { n = ns.pop() + max(maxn, n); maxn = maxns.pop() }
+			if (z == cs.len) z = 0; return next()
+		}
+		if (match(/^:is\(/   )) {
+			cs.push(1); ns.push(n); maxns.push(maxn); n = 0; maxn = 0
+			return next()
+		}
+		if (match(/^:not\(/  )) { cs.push(0); return next() }
+		if (match(/^:where\(/)) { if (!z) z = cs.len; cs.push(0); return next() }
+		if (match(/^,/       )) { maxn = max(maxn, n); n = 0; return next() }
+		if (match(/^\[[^\]]*\]/) || match(/^[\.:#]?[:a-zA-Z\-_][a-zA-Z0-9\-_]*/)) {
+			if (!z)
+				n += (sm[0] == '#' && 10 || sm[0] == '.' && 1 || sm[0] == ':' && 1 || .1)
+			return next()
+		}
+		warn('invalid selector: '+s0, s)
+	}
+	return next()
+}
+
+function css_report_specificity(file, max_spec) {
+	for (let ss of document.styleSheets) {
+		if (!((ss.href || '').ends(file || '')))
+			continue
+		for (let r of ss.cssRules) {
+			let s = r.selectorText
+			if (!isstr(s))
+				continue
+			let spec = css_selector_specificity(s)
+			if (spec > max_spec)
+				debug('CSS spec', spec, s)
+		}
+	}
+}
+
+/* composable CSS ------------------------------------------------------------
+
+	example.css:
+		.foo { }
+		.bar { }
+		.baz { --inc: foo bar; }  # gets all properties from .foo and .bar
+
+*/
+
+on_dom_load(function() {
+
+	css_report_specificity('x-widgets.css', 2)
+
+	let t0 = time()
+
+	let n = 0
+	let class_rules = obj()
+	for (let ss of document.styleSheets) {
+		for (let r of ss.cssRules) {
+			let s = r.selectorText // this is slow on Chrome :(
+			if (!isstr(s))
+				continue
+			let m = s.match(/^\.([a-zA-Z0-9\-_]+)$/) // simple class
+			if (!m)
+			 	continue
+			class_rules[m[1]] = r
+			n++
+		}
+	}
+
+	let t1 = time()
+
+	for (let ss of document.styleSheets) {
+		for (let r of ss.cssRules) {
+			let sm = r.styleMap
+			if (!sm) continue
+			let inc = sm.get('--inc') // this is slow on Chrome :(
+			inc = inc && inc[0]
+			if (!inc) continue
+			for (let s of words(inc)) {
+				let cr = class_rules[s]
+				if (cr) {
+					debug('CSS', s, '->', r.selectorText)
+					for (let [k, v] of cr.styleMap)
+						r.style[k] = v
+				} else {
+					warn('class not found: '+s)
+				}
+			}
+		}
+	}
+
+	let t2 = time()
+
+	debug('CSS --inc', n,
+		((t1 - t0) * 1000).dec(0)+'ms',
+		((t2 - t1) * 1000).dec(0)+'ms')
 
 })
 
