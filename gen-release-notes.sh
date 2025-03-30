ec1635f18807e9103bd5bcb71276d17f082e8dd4
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2025-03-30 22:59:23 +0300

    unimportant

--- gen-release-notes.sh
+++ gen-release-notes.sh
@@ -3,7 +3,7 @@ V1=$(git tag --sort=-committerdate | head -1)
 V0=$(git tag --sort=-committerdate | head -2 | awk '{split($0, tags, "\n")} END {print tags[1]}')
 CHANGES=$(git log --pretty="- %s" $V1...$V0 | grep -v unimportant)
 printf "
-## Changes
+## Changes between $V0 and $V1
 
 $CHANGES
 
