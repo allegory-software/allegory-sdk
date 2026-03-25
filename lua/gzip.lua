9a66ec6cef3ecbacc5661e217280f10400a92447
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2026-03-26 01:11:29 +0200

    gzip_state.b

--- lua/gzip.lua
+++ lua/gzip.lua
@@ -8,9 +8,11 @@ DEFLATE ----------------------------------------------------------------------
 
 gzip_state(gz) -> gz
 	OPTIONS
-		gz:write(buf, sz) -> true | nil,err
+		gz.write(buf, sz) -> [false,err]
+			* optional. if not used, data is accumulated in string_buffer gz.b
 			* allowed to yield.
-			* signal abort by returning `nil,err`.
+			* called with (nil, 'eof') on finish.
+			* signals abort by returning `false,err`.
 			* errors are not caught.
 		* bufsize: output buffer size (64K).
 		* format: 'gzip' (default), 'zlib' or 'raw'.
@@ -19,7 +21,9 @@ gzip_state(gz) -> gz
 	API
 		gz:[try_]push(s | buf,len | nil,'eof') -> true,'more'|'eof' | nil,err
 		gz:[try_]finish() -> true,'eof' | nil,err
+		gz:reset()
 		gz:free()
+		gz.b         string buffer with all the data if gz.write is not given.
 
 gzip   (s | buf,len) -> string_buffer
 gunzip (s | buf,len) -> string_buffer
@@ -78,6 +82,8 @@ int  inflateEnd   (z_stream*);
 int  inflateInit2_(z_stream*, int windowBits, const char* version, int stream_size);
 int  deflate      (z_stream*, int flush);
 int  deflateEnd   (z_stream*);
+int  inflateReset (z_stream*);
+int  deflateReset (z_stream*);
 int  deflateInit2_(z_stream*, int level, int method, int windowBits, int memLevel,
 	int strategy, const char *version, int stream_size );
 
@@ -87,7 +93,7 @@ unsigned long crc32   (unsigned long crc,   const char *buf, unsigned len );
 
 function gzip_state(gz)
 
-	local bufsize = gz.bufsize or 64 * 1024
+	local min_bufsize = gz.bufsize or 64 * 1024
 	--range 8..15; 0=use-value-in-zlib-header; see gzip manual.
 	local windowBits = gz.windowBits or C.Z_MAX_WBITS
 	local format = gz.format or 'gzip'
@@ -113,38 +119,50 @@ function gzip_state(gz)
 		error(str(C.zError(ret)))
 	end
 	gc(strm, flate_end)
+	local b = string_buffer()
+	gz.b = b
 	local function free()
 		flate_end(gc(strm, nil))
 		strm = nil
 	end
 
-	local buf = u8a(bufsize)
+	local buf, bufsize = b:reserve(min_bufsize)
 	strm.next_out, strm.avail_out = buf, bufsize
 	strm.next_in, strm.avail_in = nil, 0
 
 	function gz:try_push(data, size)
+		local eof = data == nil and size == 'eof'
+		if not strm and eof then return true, 'eof' end --pass-through eof
 		assert(strm, 'closed')
-		local flush = data == nil and size == 'eof' and C.Z_FINISH or C.Z_NO_FLUSH
+		local flush = eof and C.Z_FINISH or C.Z_NO_FLUSH
 		size = data and (size or #data) or 0
 		strm.next_in = data
 		strm.avail_in = size
 		while true do
 			local ret = flate(strm, flush)
 			if not (ret == 0 or ret == C.Z_STREAM_END) then
-				free()
 				return nil, str(C.zError(ret))
 			end
 			if strm.avail_out < bufsize then
-				local ok, err = gz.write(buf, bufsize - strm.avail_out)
+				b:commit(bufsize - strm.avail_out)
+				local ok, err
+				if gz.write then
+					local p, len = b:ref()
+					ok, err = gz.write(p, len)
+					b:reset()
+				end
+				buf, bufsize = b:reserve(min_bufsize)
 				strm.next_out, strm.avail_out = buf, bufsize
 				if ok == false then
-					free()
 					return nil, err
 				end
 			end
 			if ret == C.Z_STREAM_END then
-				gz.write(nil, 'eof')
-				free()
+				local ok, err
+				if gz.write then
+					ok, err = gz.write(nil, 'eof')
+				end
+				if ok == false then return nil, err end
 				return true, 'eof'
 			end
 			if strm.avail_in == 0 then break end
@@ -152,13 +170,31 @@ function gzip_state(gz)
 		return true, 'more'
 	end
 	function gz:push(data, size)
-		return select(2, assert(self:try_push(data, size)))
+		local ok, msg = self:try_push(data, size)
+		if not ok then
+			free()
+			error(msg)
+		end
+		return msg
 	end
 	function gz:try_finish()
 		return self:try_push(nil, 'eof')
 	end
 	function gz:finish()
 		return self:push(nil, 'eof')
+	end
+
+	local flate_reset = gz.op == 'compress' and C.deflateReset or C.inflateReset
+	function gz:reset()
+		assert(strm, 'closed')
+		local ret = flate_reset(strm)
+		if ret ~= 0 then
+			error(str(C.zError(ret)))
+		end
+		b:reset()
+		buf, bufsize = b:reserve(min_bufsize)
+		strm.next_out, strm.avail_out = buf, bufsize
+		strm.next_in, strm.avail_in = nil, 0
 	end
 
 	function gz:free()
@@ -171,19 +207,12 @@ end
 
 local function gzip_unzip(op, data, size)
 	local gz = gzip_state{op = op}
-	local b = string_buffer()
-	function gz.write(data, size)
-		if size == 'eof' then return end
-		if isstr(data) then
-			b:put(data)
-		else
-			b:putcdata(data, size)
-		end
-	end
 	gz:push(data, size)
-	if op == 'compress' then gz:finish() end
+	if op == 'compress' then
+		gz:finish()
+	end
 	gz:free()
-	return b
+	return gz.b
 end
 function gzip   (...) return gzip_unzip('compress', ...) end
 function gunzip (...) return gzip_unzip('decompress', ...) end
