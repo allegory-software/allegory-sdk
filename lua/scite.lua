d9a8463996e21dbfe8a75287820602ad58ef9219
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2026-05-02 23:59:36 +0300

    .

--- lua/scite.lua
+++ lua/scite.lua
@@ -1,5 +1,6 @@
 io.stdout:setvbuf'no'
 io.stderr:setvbuf'no'
+if not arg[0] then return end
 local script_dir = arg[0]:gsub('[/]?[^/]+$', '')
 local tests_dir = script_dir..'/../tests'
 package.path = package.path..';'..tests_dir..'/?.lua'
