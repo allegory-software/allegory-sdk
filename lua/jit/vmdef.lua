541fb1bae7f63e0d84e53c7ae0ced9f34390f981
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2026-06-02 16:28:53 +0300

    luajit update leftovers

--- lua/jit/vmdef.lua
+++ lua/jit/vmdef.lua
@@ -1,6 +1,6 @@
 -- This is a generated file. DO NOT EDIT!
 
-assert(require("jit").version == "LuaJIT 2.1.1772649449", "LuaJIT core/library version mismatch")
+assert(require("jit").version == "LuaJIT 2.1.1780393537", "LuaJIT core/library version mismatch")
 
 return {
 
